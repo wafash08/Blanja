7e819fc0fd027234410b1cc26cd99099fafca622
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2024-05-22 14:28:18 +0700

    navbar component

--- BlanjaFE/src/App.jsx
+++ BlanjaFE/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Container from './components/base/Container';
+import Navbar from './components/modules/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<header className='fixed top-0 left-0 w-full h-[100px] z-50 bg-white flex items-center shadow-[0_6px_40px_0_#ADADAD40]'>
+				<Container className='flex items-center'>
+					<Navbar hasLoggedIn={true} inHomePage={true} />
+				</Container>
+			</header>
+		</>
+	);
 }
 
-export default App
+export default App;
