8ec480d0a39526799cdd0ac03b837169cc0fe9e6
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2024-05-24 11:22:16 +0700

    add layout component

---
+++ BlanjaFE/src/components/modules/Layout.jsx
@@ -0,0 +1,23 @@
+import { Outlet, useLocation } from 'react-router-dom';
+import Container from '../base/Container';
+import Navbar from './Navbar';
+
+export default function Layout() {
+	const { pathname } = useLocation();
+	const pathnames = pathname.split('/');
+	const inProductsPage = pathnames.includes('products');
+	const inCategoriesPage = pathnames.includes('categories');
+	const inHomePage = !inCategoriesPage && !inProductsPage;
+	return (
+		<>
+			<header className='fixed top-0 left-0 w-full h-[100px] z-50 flex items-center bg-white shadow-[0_6px_40px_0_#ADADAD40]'>
+				<Container>
+					<Navbar hasLoggedIn={true} inHomePage={inHomePage} />
+				</Container>
+			</header>
+			<main className='mt-32'>
+				<Outlet />
+			</main>
+		</>
+	);
+}
