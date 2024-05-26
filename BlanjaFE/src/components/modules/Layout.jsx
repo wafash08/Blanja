94ab0d4826396f2dd3b1c3267d348bb051bb85ec
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2024-05-26 22:05:29 +0700

    fix navbar

--- BlanjaFE/src/components/modules/Layout.jsx
+++ BlanjaFE/src/components/modules/Layout.jsx
@@ -2,9 +2,11 @@ import { Outlet } from 'react-router-dom';
 import Container from '../base/Container';
 import Navbar from './Navbar';
 import { getTokenFromLocalStorage } from '../../utils';
+import { useState } from 'react';
 
 export default function Layout() {
-	const token = getTokenFromLocalStorage();
+	const [token, setToken] = useState(() => getTokenFromLocalStorage());
+	console.log('token >> ', token);
 	return (
 		<>
 			<header className='fixed top-0 left-0 w-full h-[100px] z-50 flex items-center bg-white shadow-[0_6px_40px_0_#ADADAD40]'>
